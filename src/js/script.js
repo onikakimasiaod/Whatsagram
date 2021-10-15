9919ac6b871e0e74a530c66c39e813028801e120
oasiao <74007996+oasiao@users.noreply.github.com>
2021-10-15 23:59:49 +0200

    keyboard and input

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,4 @@
-/*
-let keyboard = document.getElementsByClassName("keyboard");
-
-Array.from(keyboard).forEach(function(letter) {
-    letter.addEventListener('click', takingValue(letter));
-    console.log("Hello");
-});
-
-function typing(){
-    let keyboard = document.getElementsByClassName("keyboard");
-    for (let i = 0; i < keyboard.length; i++) {
-        keyboard[i].addEventListener('click', takingValue(keyboard[i]));
-    }
-}
-
-function takingValue(letra){
-    console.log(letra.value);
-    document.getElementById("container").innerHTML+=letra.value;
-}
-*/
-
+//KEYBOARD CODE
 let keyboard = document.querySelectorAll('.keyboard');
 keyboard.forEach(letter => {letter.addEventListener('click', event => {
-    //handle click
-    console.log(letter.value);
-    document.getElementById("container").innerHTML+=letter.value;})})
+    document.getElementById("text").innerHTML+=letter.value;})})
