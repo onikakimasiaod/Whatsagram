0eebb0164092d510dad5bf78d37c99317e0350b4
oasiao <74007996+oasiao@users.noreply.github.com>
2021-10-17 16:48:42 +0200

    keyboard js not finished yet

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,44 @@
 //KEYBOARD CODE
-let keyboard = document.querySelectorAll('.keyboard');
+var keyboard = document.querySelectorAll('.keyboard');
 keyboard.forEach(letter => {letter.addEventListener('click', event => {
-    document.getElementById("text").innerHTML+=letter.value;})})
+    document.getElementById("text").innerHTML+=letter.value;})});
+
+//FOR SPECIAL CHARACTERS
+let special = document.querySelectorAll('.special');
+special.forEach(letter => {letter.addEventListener('click', event => {
+    document.getElementById("text").innerHTML+=letter.value;})});
+
+let space = document.querySelectorAll('.space');
+space.forEach(letter => {letter.addEventListener('click', event => {
+    document.getElementById("text").innerHTML+=letter.value;})});
+
+let intro = document.querySelectorAll('.intro');
+intro.forEach(letter => {letter.addEventListener('click', event => {
+    document.getElementById("text").innerHTML+=letter.value;})});
+
+let del = document.querySelectorAll('.delete');
+del.forEach(letter => {letter.addEventListener('click', event => {
+    document.getElementById("text").innerHTML+=letter.value;})});
+
+//KEYBOARD TO MINUS TO MAYUS
+function toUpper() {
+    document.getElementById('keyboardMinus').style.display="none";
+    document.getElementById('keyboardMayus').style.display="block";
+    document.getElementById('keyboardNum').style.display="none";
+}
+
+function toLower(){
+    document.getElementById('keyboardMinus').style.display="block";
+    document.getElementById('keyboardMayus').style.display="none";
+    document.getElementById('keyboardNum').style.display="none";
+}
+
+function toNum(){
+    document.getElementById('keyboardMinus').style.display="none";
+    document.getElementById('keyboardMayus').style.display="none";
+    document.getElementById('keyboardNum').style.display="block";
+}
+
+function toSymb(){
+
+}
