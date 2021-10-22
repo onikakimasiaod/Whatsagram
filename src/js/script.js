93532093d94b8d4e09c719cf653bd7ab80996e55
oasiao <74007996+oasiao@users.noreply.github.com>
2021-10-22 15:41:12 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ function emoji(){
 }
 
 
-/*TODO 5 CEC: LA TECLA C ESBORRA EL TEXT DE LA TEXT AREA Y CE ESBORRA LA DARRERA PARAULA (TRIM)*/
+/*TODO 5 CEC: LA TECLA C ESBORRA EL TEXT DE LA TEXT AREA Y CE ESBORRA LA DARRERA PARAULA (TRIM) ✅*/
 function deleteAll() {
     document.getElementById("texto").innerHTML = "";
 }
@@ -20,7 +20,7 @@ function deleteWord(){
 }
 
 
-/*TODO 6 LA TECLA <-- ESBORRA EL DARRER CARÀCTER I--> ESBORRA EL PRIMER CARÀCTER*/
+/*TODO 6 LA TECLA <-- ESBORRA EL DARRER CARÀCTER I--> ESBORRA EL PRIMER CARÀCTER ✅*/
 
 function deleteChar() {
     let msg = document.getElementById("texto").value;
@@ -37,7 +37,7 @@ function deleteFirst(){
 
 
 
-/*TODO 2 TEXT AREA: LLETRES*/
+/*TODO 2 TEXT AREA: LLETRES ✅*/
 //KEYBOARD CODE
 let keyboard = document.querySelectorAll('.keyboard');
 keyboard.forEach(letter => {letter.addEventListener('click', event => {
@@ -48,7 +48,7 @@ let special = document.querySelectorAll('.special');
 special.forEach(letter => {letter.addEventListener('click', event => {
     document.getElementById("texto").innerHTML+=letter.value; })});
 
-/*TODO 8 SPACE BUTTON*/
+/*TODO 8 SPACE BUTTON ✅*/
 let space = document.querySelectorAll('.space');
 space.forEach(letter => {letter.addEventListener('click', event => {
     document.getElementById("texto").innerHTML += letter.value;
@@ -56,8 +56,9 @@ space.forEach(letter => {letter.addEventListener('click', event => {
 
 
 /*TODO 9 NEW LINE BUTTON*/
+//funciona en el text area pero no al enviar
 function line(){
-    document.getElementById("texto").innerHTML+="\n";
+    document.getElementById("texto").innerHTML+=`\n`;
 }
 //KEYBOARD TO MINUS TO
 /*TODO 7 MAJUSCULES DESACTIVAT/ 1 CARACTER EN MAJUSCULA / MAJUSCULES PERMANENTS */
@@ -88,7 +89,7 @@ function line(){
         document.getElementById('keyboardSymb').style.display = "block";
     }
 
-/*TODO 3 SEND BUTTON*/
+/*TODO 3 SEND BUTTON ✅*/
 
 /*
 * <div class="message">
@@ -98,8 +99,7 @@ function line(){
 */
     function send(){
         let today = new Date();
-        let hour = today.getHours()+':'+today.getMinutes();
-
+        let hour = ("0"+today.getHours()).slice(-2)+':'+("0"+today.getMinutes()).slice(-2);
         //message with class without hour
         let myDiv = document.createElement("div");
         let clase = document.createAttribute("class");
@@ -115,7 +115,11 @@ function line(){
 
         //ya hemos creado <p class="textContent"></p>
         //a continuacion creamos el texto
-        let message = document.createTextNode(document.getElementById("texto").value);
+        let content = document.getElementById("texto").value
+        //TODO ARREGLAR LA LINEA NUEVA!!!!! (ARREGLADO PERO UNA CACA)
+        content = content.replace("\n"," <br> ");
+        let message = document.createTextNode(content);
+
         p1.appendChild(message); //mensaje hecho <p class="textContent">MESSAGE</p>
         /*document.getElementsByClassName("message")[divsNum].appendChild(p1);*/
         //segunda p con time
@@ -130,21 +134,20 @@ function line(){
         p2.appendChild(hourMinutes); //mensaje hecho <p class="time">hourMinutes</p>
 
         let divsNum = document.querySelectorAll('.message').length;
-        console.log(divsNum);
         //a continuación, añadimos la p dentro del div
 
         myDiv.appendChild(p1);
         myDiv.appendChild(p2);
         /*document.getElementsByClassName("message")[divsNum].appendChild(p2);*/
 
-        console.log(p1);
-        console.log(p2);
-        console.log(myDiv);
         //añado el div
         document.getElementsByClassName("app-messagingArea")[0].appendChild(myDiv);
+        document.getElementsByClassName("message")[divsNum].innerHTML = content;
         document.getElementById("texto").innerHTML="";
 
     }
+
+    //TODO ARREGLAR LOS MARGINS DEL BACKGROUND DE LOS MENSAJES
 
 
 
@@ -156,3 +159,4 @@ function fechaActual(){
     let fecha = today.getDay()+" "+monthNames[today.getMonth()]+" "+today.getFullYear();
     document.getElementById("date").innerHTML=fecha;
 }
+/*TODO 19 FORMATO DE LA HORA ✅*/
